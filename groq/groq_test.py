1f7831975a5fdb61f908bf5f0d0622d41f20da99
Kerem Yıldız <yildizkerem8085@gmail.com>
2024-12-28 18:44:41 +0300

    removed env

--- groq/groq_test.py
+++ groq/groq_test.py
@@ -17,20 +17,22 @@ groq_api_key='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'
 
 if "vector" not in st.session_state:
   st.session_state.embeddings=OllamaEmbeddings()
-  st.session_state.loader=WebBaseLoader("https://docs.smith.langchain.com/")
+  st.session_state.loader=WebBaseLoader("https://gsb.gov.tr/")
   st.session_state.docs=st.session_state.loader.load()
   st.session_state.text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200) 
   st.session_state.final_documents=st.session_state.text_splitter.split_documents(st.session_state.docs[:50]) 
   st.session_state.vectors=FAISS.from_documents(st.session_state.final_documents,st.session_state.embeddings)
 
 
-st.title("ChatGroq Demo")
+st.title("soruCan Demo")
 llm=ChatGroq(groq_api_key=groq_api_key,
 model_name="mixtral-8x7b-32768")
 prompt=ChatPromptTemplate.from_template(
 """
-Answer the questions based on the provided context only.
-Please provide the most accurate response based on the question
+Sen gençlik bilgilendirme merkezinde çalışan bir uzman gibi davranıyorsun.
+Sorulara Gençlik Bilgilendirme Merkezi uzmanı gibi cevap ver.
+Sorulara sadece sağlanan dosya üzerinden cevap ver.
+Lütfen Soruya göre en doğru cevabı verin.
 <context>
 {context}
 <context>
@@ -41,7 +43,7 @@ document_chain = create_stuff_documents_chain(llm, prompt)
 retriever = st.session_state.vectors.as_retriever()
 retrieval_chain = create_retrieval_chain(retriever, document_chain)
 
-prompt=st.text_input("Input you prompt here")
+prompt=st.text_input("Sorunuzu Buraya Girin")
 if prompt:
     start=time.process_time()
     response=retrieval_chain.invoke({"input":prompt})
